b36fdda94f797d4382e73b78a7ed1b85efd42034
Kenta <102387086+KentaKK@users.noreply.github.com>
2024-01-05 19:22:25 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,29 +1,29 @@
-CREATE TABLE `bwh_bans` (
+CREATE TABLE IF NOT EXISTS `bwh_bans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `receiver` text NOT NULL,
   `sender` varchar(60) NOT NULL,
-  `length` datetime NULL,
+  `length` datetime DEFAULT NULL,
   `reason` text NOT NULL,
-  `unbanned` TINYINT NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-);
+  `unbanned` tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
-CREATE TABLE `bwh_warnings` (
+CREATE TABLE IF NOT EXISTS `bwh_warnings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `receiver` text NOT NULL,
   `sender` varchar(60) NOT NULL,
   `message` text NOT NULL,
-  PRIMARY KEY (`id`)
-);
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
-CREATE TABLE `bwh_identifiers` (
-  `steam` VARCHAR(60) NOT NULL,
-  `license` VARCHAR(60) NOT NULL,
-  `ip` VARCHAR(60) NOT NULL,
-  `name` VARCHAR(128) NOT NULL,
-  `xbl` VARCHAR(60) NULL,
-  `live` VARCHAR(60) NULL,
-  `discord` VARCHAR(60) NULL,
-  `fivem` VARCHAR(60) NULL,
-  PRIMARY KEY (`steam`)
-);
+CREATE TABLE IF NOT EXISTS `bwh_identifiers` (
+  `steam` varchar(60) NOT NULL,
+  `license` varchar(60) NOT NULL,
+  `ip` varchar(60) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `xbl` varchar(60) DEFAULT NULL,
+  `live` varchar(60) DEFAULT NULL,
+  `discord` varchar(60) DEFAULT NULL,
+  `fivem` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`steam`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
